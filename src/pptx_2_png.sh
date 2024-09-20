70f4deb4a67dd43bf119f545711bde6b59f41046
Asi0Flammeus <asi0.flammeus@pm.me>
2024-09-21 00:37:32 +0200

    update convert to imagemagick v7

--- src/pptx_2_png.sh
+++ src/pptx_2_png.sh
@@ -30,7 +30,7 @@ echo "Converting PDF to PNG..."
 for ((i=0; i<NUM_PAGES; i++))
 do
     echo -ne "Converting page $((i+1)) of $NUM_PAGES...\r"
-    convert -density 300 "${PDF_PATH}[$i]" -quality 100 "${OUTPUT_DIR}/${BASE_NAME}_slide_$(printf "%03d" $((i+1))).png"
+    magick convert -density 300 "${PDF_PATH}[$i]" -quality 100 "${OUTPUT_DIR}/${BASE_NAME}_slide_$(printf "%03d" $((i+1))).png"
 done
 echo -ne "\nConversion to PNG completed. Images saved in ${OUTPUT_DIR}\n"
 
