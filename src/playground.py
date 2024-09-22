3572242d78cf366e0e5deffe81cbe874941ef24f
Asi0Flammeus <asi0.flammeus@pm.me>
2024-09-22 13:13:01 +0200

    add playground script

--- src/playground.py
+++
@@ -1,8 +0,0 @@
-from config import VOICE_IDS
-from mp3_2_txt import TranscriptionModel 
-from txt_2_mp3 import text_to_speech
-from txt_translation import save_translation, translate_txt_to
-
-path = "../test/scu101/fr/v001/2.1/slides/2.1.01_rogzy.txt"
-text_to_speech(path, VOICE_IDS["rogzy"])
-
