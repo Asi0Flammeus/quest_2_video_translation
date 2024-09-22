9a52c023362ff796dcc41a4bbcd4c57d607100df
Asi0Flammeus <asi0.flammeus@pm.me>
2024-09-22 13:12:15 +0200

    update config for elevenlabs

--- src/config.py
+++ src/config.py
@@ -7,12 +7,14 @@ import json
 parent_dir = Path(__file__).parent.parent
 load_dotenv(dotenv_path=parent_dir / '.env')
 
-ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
-CHUNK_SIZE = 1024
-VOICE_IDS = os.getenv("voice_ids")
-
 API_KEY_ANTHROPIC = os.getenv('API_KEY_ANTHROPIC')
 anthropic_client = anthropic.Anthropic(api_key=API_KEY_ANTHROPIC)
-voice_ids_json = os.linesep.join(os.getenv('VOICE_IDS').splitlines())
 
+API_KEY_ELEVENLABS = os.getenv("API_KEY_ELEVENLABS")
+HEADERS = {
+    "Accept": "application/json",
+    "xi-api-key": API_KEY_ELEVENLABS,
+    "Content-Type": "application/json"
+}
+voice_ids_json = os.linesep.join(os.getenv('VOICE_IDS').splitlines())
 voice_ids = json.loads(voice_ids_json)
